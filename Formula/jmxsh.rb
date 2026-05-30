8acc22e21239ccf22a40b634dfb41d49534e87e5
nyg <784863+nyg@users.noreply.github.com>
2026-05-30 11:51:04 +0000

    jmxsh: update 1.3.1 bottle.

--- Formula/jmxsh.rb
+++ Formula/jmxsh.rb
@@ -11,9 +11,9 @@ class Jmxsh < Formula
   end
 
   bottle do
-    root_url "https://github.com/nyg/homebrew-tap/releases/download/jmxsh-1.3.0"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "ee02a1146e1b4bf553f41b476c5ceee30cb3cc5839244f4c607b25b753427bb1"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "9190fc33e6ceedc3b84926126048abd97cb9d608c1c07e5dd83e356aa81afe53"
+    root_url "https://github.com/nyg/homebrew-tap/releases/download/jmxsh-1.3.1"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "9c21e36c60b5e60496a0f5c025c4e23b2d82ba8f820fb283a8647b69be5b2947"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "b688de0854c251b617d3ae9739515d410d8f0b310e13cbba37421e8284b4f039"
   end
 
   depends_on "openjdk@25"
