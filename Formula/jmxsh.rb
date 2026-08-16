95020f544d215df69bbc5a8d0c449b237f067ead
nyg <nyg@users.noreply.github.com>
2026-08-16 12:04:14 +0200

    jmxsh 1.5.2 (#53)
    Signed-off-by: nyg <784863+nyg@users.noreply.github.com>
    Co-authored-by: nyg <784863+nyg@users.noreply.github.com>

--- Formula/jmxsh.rb
+++ Formula/jmxsh.rb
@@ -1,8 +1,8 @@
 class Jmxsh < Formula
   desc "Command-line JMX client for monitoring and managing Java applications"
   homepage "https://github.com/nyg/jmxsh"
-  url "https://github.com/nyg/jmxsh/releases/download/v1.5.1/jmxsh-1.5.1.jar"
-  sha256 "df67ac21295e049e0c6207b2108824967c7b36dad986f0908fa39a88174d13f3"
+  url "https://github.com/nyg/jmxsh/releases/download/v1.5.2/jmxsh-1.5.2.jar"
+  sha256 "c7c1f9da8a8f8f26d6c7f1a57f0494aef32cb6f0c600e4d997ee0116eb97436a"
   license "Apache-2.0"
 
   livecheck do
@@ -11,9 +11,9 @@ class Jmxsh < Formula
   end
 
   bottle do
-    root_url "https://github.com/nyg/homebrew-tap/releases/download/jmxsh-1.5.1"
-    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "6016f58d87c3faeacbbf192b6eca94297ae22adba272dd611a78cb50643438d0"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "6bba380aa0da0746c9900b6e081349d0170de9fe7d5ee30d62ee0b6f06f5b70b"
+    root_url "https://github.com/nyg/homebrew-tap/releases/download/jmxsh-1.5.2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "0794a5360c086f11db6edcbf2e9fbd9bf59d0addfde8646500c58b4cc9d88535"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "c0622b15e5573cc4a01dc0ebc113c5b03f63defac690aabc191e7ec48903c0d5"
   end
 
   depends_on "openjdk@25"
